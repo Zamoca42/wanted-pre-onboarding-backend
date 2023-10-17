052c603359b265104523dabe8d7f90940b2e98cf
Zamoca <suntail2002@naver.com>
2023-10-18 03:20:15 +0900

    refactor: 불필요한 로직 삭제

--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
-import { CommonResponse } from 'src/common/common.response';
 import { Company } from './entities/company.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
